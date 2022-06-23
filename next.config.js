9408f425929e7ae0cac8d552d04488c7cd8f31aa
Jobin T V <jobinvarghese18@gmail.com>
2022-06-23 11:36:43 +0530

    feat: initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['upload.wikimedia.org', 'images.unsplash.com','image.tmdb.org']
+  },
+  swcMinify: false,
+ env:{
 
-module.exports = nextConfig
+ }
+};
+
+module.exports = nextConfig;
+
+    // path: 'https://upload.wikimedia.org/wikipedia/',
